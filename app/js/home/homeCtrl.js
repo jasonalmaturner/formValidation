ff44e3f64d7f8c80c08d517a639dac019de86d52
jasonalmaturner <jasonalmaturner@gmail.com>
2014-10-02 17:32:24 -0600

    almost finished working on the form validation. Going to start setting up the node server.

--- app/js/home/homeCtrl.js
+++ app/js/home/homeCtrl.js
@@ -1,6 +1,15 @@
 var app = angular.module('devSocial');
 
 app.controller('homeCtrl', function($scope) {
-
+	$scope.login = true;
+	$scope.signup = false;
+	$scope.loginToggle = function(){
+		$scope.login = true;
+		$scope.signup = false;
+	}
+	$scope.signupToggle = function(){
+		$scope.signup = true;
+		$scope.login = false;
+	}
 
 })
